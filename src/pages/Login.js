67b4856deb6b78fe5d6d13f6f01c76e26179bcb0
shubhamkv <shubh082002@gmail.com>
2023-07-17 02:05:07 +0530

    update ui of login page

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -6,12 +6,47 @@ const Login = () => {
   const { loginWithRedirect } = useAuth0();
   return (
     <Wrapper>
-      <div className="container">
-        <img src={loginImg} alt="github users" />
-        <h1>github user</h1>
-        <button className="btn" onClick={loginWithRedirect}>
-          login / sign up
-        </button>
+      <nav
+        style={{
+          width: "var(--fluid-width)",
+          maxWidth: "var(--max-width)",
+          margin: "0 auto",
+          height: "var(--nav-height)",
+          display: "flex",
+          alignItems: "center",
+          color: "hsl(185, 84%, 25%)",
+        }}
+      >
+        <h1>gitpedia </h1>
+      </nav>
+      <br />
+      <div
+        style={{
+          width: "var(--fluid-width)",
+          maxWidth: "var(--max-width)",
+          margin: "0 auto",
+        }}
+      >
+        <div>
+          <h2>
+            Search <span style={{ color: "hsl(185, 77%, 34%)" }}>github</span>{" "}
+            users
+          </h2>
+          <br />
+          <h4>
+            register to know the statistics of your favourite developer and top
+            contributor across the world
+          </h4>
+          <br />
+          <button className="btn" onClick={loginWithRedirect}>
+            login / sign up
+          </button>
+        </div>
+        <img
+          src={loginImg}
+          alt="github users"
+          style={{ width: "100%", display: "block", objectFit: "cover" }}
+        />
       </div>
     </Wrapper>
   );
@@ -19,7 +54,7 @@ const Login = () => {
 const Wrapper = styled.section`
   min-height: 100vh;
   display: grid;
-  place-items: center;
+  //place-items: center;
   .container {
     width: 90vw;
     max-width: 600px;
